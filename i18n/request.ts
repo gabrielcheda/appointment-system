2e6577b479b52a36e49f8560d39a789093185326
Gabriel Cheda <gabriel.cheda@hotmail.com>
2025-11-03 15:31:29 -0300

    teste

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,13 +1,12 @@
 import { getRequestConfig } from 'next-intl/server'
-import { routing } from './routing'
 
 export const locales = ['pt-BR', 'en', 'es'] as const
 export type Locale = (typeof locales)[number]
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming locale parameter is valid
-  if (!locale || !routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale
+  if (!locale || !locales.includes(locale as Locale)) {
+    locale = 'pt-BR'
   }
 
   return {
